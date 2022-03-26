2df6ea665236219ff91a2ce6f1352d1afb7fae42
20mccoya <77861714+20mccoya@users.noreply.github.com>
2022-03-25 21:17:13 -0400

    Update BLOCK1.m

--- BLOCK1.m
+++ BLOCK1.m
@@ -201,8 +201,7 @@ l = [l11 l12 l13 l14 l15 l16 l21 l22 l23 l24 l25 l26 l31 l32 l33 l34 l35 l36 l41
 % Draw images to the screen, uses the two vectors to assign each image
 % a particular spot on the screen
 Screen('DrawTextures', window, P, [],l, 0); 
-% Flip image to the screen 
-Screen('Flip', window); 
+
    
 % Flip to the screen
 Screen('Flip', window);
